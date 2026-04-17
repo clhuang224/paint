07b605c2c9e2712776690261b5ad71cd29026b94
Lynn <clhuang224@gmail.com>
2026-04-17 19:03:13 +0800

    feat: register component

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
 import Alpine from 'alpinejs'
 
 globalThis.Alpine = Alpine
+
+Alpine.data('paintApp', () => ({
+    size: '10px',
+    color: '#000000',
+    startDrawing(event) {
+        // TODO: implement dragging logic
+    },
+    draw(event) {
+        // TODO: implement drawing logic
+    },
+    stopDrawing() {
+        // TODO
+    },
+    exportImage() {
+        // TODO
+    },
+    clearCanvas() {
+        // TODO
+    },
+    undoAction() {
+        // TODO
+    },
+    redoAction() {
+        // TODO
+    },
+}))
+
 Alpine.start()
