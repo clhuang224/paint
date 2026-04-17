a7b837a82c9b9effb41c412b85aed42288033197
Lynn <clhuang224@gmail.com>
2026-04-17 19:33:21 +0800

    feat: draw

--- main.js
+++ main.js
@@ -5,14 +5,28 @@ globalThis.Alpine = Alpine
 Alpine.data('paintApp', () => ({
     size: 10,
     color: '#000000',
-    startDrawing(event) {
-        // TODO: implement dragging logic
+    canvas: null,
+    isDrawing: false,
+    startDrawing() {
+        this.isDrawing = true
     },
     draw(event) {
-        // TODO: implement drawing logic
+        if (!this.isDrawing) return
+
+        const rect = this.canvas.getBoundingClientRect()
+        const scaleX = this.canvas.width / rect.width
+        const scaleY = this.canvas.height / rect.height
+        const x = (event.clientX - rect.left) * scaleX
+        const y = (event.clientY - rect.top) * scaleY
+
+        const ctx = this.canvas.getContext('2d')
+        ctx.fillStyle = this.color
+        ctx.beginPath()
+        ctx.arc(x, y, this.size / 2, 0, Math.PI * 2)
+        ctx.fill()
     },
     stopDrawing() {
-        // TODO
+        this.isDrawing = false
     },
     exportImage() {
         // TODO
